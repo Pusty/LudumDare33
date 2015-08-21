777fb4b09d9b62596fe2293b2a6a1f84ff05e1f6
Pusty <pusty8888@gmail.com>
2015-08-21 21:40:32 +0200

    c

--- src/me/engine/entity/Status.java
+++
@@ -1,7 +0,0 @@
-package me.engine.entity;
-
-
-public enum Status{
-	NONE,SKILL1,SKILL2,UNMOVE,DMG;
-	public static int DAMAGE_COOLDOWN=240;
-}
--- src/me/engine/multiplayer/Side.java
+++
@@ -1,9 +0,0 @@
-package me.engine.multiplayer;
-
-import me.engine.main.MainClass;
-
-public abstract class  Side{
-  public abstract String getSide();
-  public abstract void sendPacket(Packet p);
-  
-}
